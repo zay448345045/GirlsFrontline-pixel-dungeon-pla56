f66ddaf385216c4ba88ac6bef4388d906ddf578b
zay448345045 <raknet@163.com>
2025-08-29 15:27:51 +0800

    Update WandOfReflectDisintegration.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/custom/testmode/WandOfReflectDisintegration.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/custom/testmode/WandOfReflectDisintegration.java
@@ -1,5 +1,8 @@
 package com.shatteredpixel.shatteredpixeldungeon.custom.testmode;
 
+import com.shatteredpixel.shatteredpixeldungeon.items.materials.MetalShard;
+import com.shatteredpixel.shatteredpixeldungeon.items.materials.GooBlob;
+
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
